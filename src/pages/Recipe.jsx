a8f99b49dc4e28a5e5989e7adc6fcbbd5edc0aa7
Max Givno <max.ryazanov@gmail.com>
2020-09-12 11:16:03 +0300

    Ingredients list fix

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,63 +1,44 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import ReactMarkdown from "react-markdown";
 
-import StarRating from "../components/StarRating";
-import Tags from "../components/Tags";
+// import StarRating from "../components/StarRating";
 import IngredientsList from "../components/IngredientsList";
-import ForkedFrom from "../components/ForkedFrom";
 
-import { AppContext } from "../AppContext";
-
-function Recipe() {
+function Recipe({ meals }) {
   const { recipeId } = useParams();
-  const { recipes } = useContext(AppContext);
 
-  const recipe = recipes.find((recipe) => recipe.id === recipeId);
+  const recipe = Object.values(meals)
+    .flat()
+    .find((recipe) => recipe.idMeal === recipeId);
 
-  const ingredients = recipe.ingredient.length !== 0 && (
-    <IngredientsList ingList={recipe.ingredient} />
-  );
-
-  const additionalIngredients = () => {
-    if (
-      recipe.ingredientGroup &&
-      recipe.ingredientGroup[0] &&
-      recipe.ingredientGroup[0].hasOwnProperty("name")
-    ) {
-      return (
-        <>
-          {recipe.ingredientGroup.map((ingList, i) => (
-            <IngredientsList key={i} ingList={ingList} />
-          ))}
-        </>
-      );
+  const ingredients = () => {
+    let arr = Object.entries(recipe);
+    let ingNames = arr.filter((item) => item[0].startsWith("strIngre"));
+    let ingMeasures = arr.filter((item) => item[0].startsWith("strMeasur"));
+    let list = [];
+    for (let i = 0; i < ingNames.length; i++) {
+      if (ingNames[i][1]) {
+        list.push(
+          <p key={i} className="ingredient">
+            {ingMeasures[i][1] + " " + ingNames[i][1]}
+          </p>
+        );
+      }
     }
+    return list;
   };
 
-  const steps = recipe.step.map((item, i) => (
-    <li key={i}>
-      <ReactMarkdown source={item.description} />
-    </li>
-  ));
-  const notes = recipe.notes && (
-    <section className="notes">
-      <h3 className="recipe-element-title">Notes:</h3>
-      <ReactMarkdown source={recipe.notes} />
-    </section>
-  );
-  const forkedFrom = <ReactMarkdown source={recipe.forked} />;
+  const steps = <p>{recipe.strInstructions}</p>;
 
   return (
     <div className="recipe">
       <section className="title">
-        <img src={recipe.image} alt={recipe.id} />
+        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
         <div className="title-info">
-          <h1>{recipe.name}</h1>
-          <p className="title-desc">{recipe.description}</p>
+          <h1>{recipe.strMeal}</h1>
           <div className="icons">
-            <StarRating rating={(Math.random() * (5 - 2) + 2).toFixed(1)} />
-            <Tags tags={recipe.tag} />
+            {/* <StarRating rating={(Math.random() * (5 - 2) + 2).toFixed(1)} /> */}
+            <p className="tag">{recipe.strCategory.toUpperCase()}</p>
           </div>
         </div>
       </section>
@@ -65,16 +46,13 @@ function Recipe() {
       <div className="recipe-body">
         <div className="main">
           <section className="ingredients">
-            {ingredients}
-            {additionalIngredients()}
+            <IngredientsList ingList={ingredients()} />
           </section>
           <section className="steps">
             <h3 className="recipe-element-title">Directions:</h3>
             <ol className="steps-list">{steps}</ol>
           </section>
         </div>
-        {notes}
-        {recipe.hasOwnProperty("forked") && <ForkedFrom source={forkedFrom} />}
       </div>
     </div>
   );
