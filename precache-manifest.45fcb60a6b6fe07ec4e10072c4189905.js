524d78eff23e975822f3e09c0cbc6ab7a0d6a8a9
Max Givno <max.ryazanov@gmail.com>
2020-09-20 12:03:33 +0300

    Updates

---
+++ precache-manifest.45fcb60a6b6fe07ec4e10072c4189905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249efea00f8bb9672217a253677e4767",
+    "url": "/food-helper/index.html"
+  },
+  {
+    "revision": "44aa712ab48e2ef8ccd5",
+    "url": "/food-helper/static/css/main.1aec4f09.chunk.css"
+  },
+  {
+    "revision": "9595a0b7ef49e3abd888",
+    "url": "/food-helper/static/js/2.20a1ad5c.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/food-helper/static/js/2.20a1ad5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44aa712ab48e2ef8ccd5",
+    "url": "/food-helper/static/js/main.395489fa.chunk.js"
+  },
+  {
+    "revision": "c0938b904cce2818ba23",
+    "url": "/food-helper/static/js/runtime-main.b630c417.js"
+  }
+]);
