481df56dc20d4e2e70890c127084e8c34fdb6460
Max Givno <max.ryazanov@gmail.com>
2020-09-20 11:50:16 +0300

    Hero section images fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className='container'>
-      <h2 className="logo"><Link to="/">Cooking</Link></h2>
+    <header className="container">
+      <h2 className="logo">
+        <Link to="/">Cooking</Link>
+      </h2>
       <ul className="menu">
         <li>
           <Link to="/">
-            <i className="fas fa-home"> Browse</i>
+            <i className="fas fa-home"></i> Browse
           </Link>
         </li>
         <li>
-          <i className="far fa-bookmark"> Bookmarks</i>
+          <i className="far fa-bookmark"></i> Bookmarks
         </li>
       </ul>
     </header>
